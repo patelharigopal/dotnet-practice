8cc59984126d71ee0889c4ef785eeca61409d329
patelharigopal <patel.harigopal@gmail.com>
2025-01-31 20:06:14 +0530

    Create Test5.cs

---
+++ LINQ and collections/Test5.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class Test5
+{
+    static async Task Main(string[] args)
+    {
+        string result = await DownloadContentAsync();
+        Console.WriteLine(result);
+        string result1 = DownloadContentAsync();
+        Console.WriteLine(result1);
+    }
+
+    static async Task<string> DownloadContentAsync()
+    {
+        using HttpClient client = new HttpClient();
+        string result = await client.GetStringAsync("http://example.com");
+        return result;
+    }
+}
