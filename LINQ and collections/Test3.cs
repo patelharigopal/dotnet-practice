2752b372219f3a72b3b487993358e73e7c291b58
patelharigopal <patel.harigopal@gmail.com>
2025-01-31 20:04:44 +0530

    Create Test3.cs

---
+++ LINQ and collections/Test3.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Threading;
+
+class Test3
+{
+    static void Main(string[] args)
+    {
+        Thread thread = new Thread(new ThreadStart(DoWork));
+        thread.Start();
+
+        Console.WriteLine("Main thread does some work, then waits.");
+        thread.Join();
+        Console.WriteLine("Background thread has completed. Main thread ends.");
+    }
+
+    static void DoWork()
+    {
+        Console.WriteLine("Background thread is working.");
+        Thread.Sleep(1000);
+        Console.WriteLine("Background thread has finished.");
+    }
+}
