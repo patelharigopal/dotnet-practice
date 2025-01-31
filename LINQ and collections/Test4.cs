b3606ce37c41973c88ebba47df7e66833c1e8140
patelharigopal <patel.harigopal@gmail.com>
2025-01-31 20:05:47 +0530

    Create Test4.cs

---
+++ LINQ and collections/Test4.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class Test4
+{
+    static async Task Main(string[] args)
+    {
+        string result = await DownloadContentAsync();
+        Console.WriteLine(result);
+    }
+
+    static async Task<string> DownloadContentAsync()
+    {
+        using HttpClient client = new HttpClient();
+        string result = await client.GetStringAsync("http://example.com");
+        return result;
+    }
+}
