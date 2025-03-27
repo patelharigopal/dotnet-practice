0a9e9ecd4083b9bde5add3f4418036f3de114b54
patelharigopal <patel.harigopal@gmail.com>
2025-03-27 12:27:42 +0530

    Create Test8.cs

---
+++ LINQ and collections/Test8.cs
@@ -0,0 +1,56 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class Test7
+{
+    async Task Method1()
+    {
+        string result1 = await DownloadContentAsync();
+        Console.WriteLine(result1);
+		
+        string result2 = DownloadContent();
+        Console.WriteLine(result2);
+		
+        string result3 = await DownloadContentAsync();
+        Console.WriteLine(result3);
+    }
+	
+	async Task Method2()
+    {
+        string result1 = await DownloadContentAsync();
+        Console.WriteLine(result1);
+		
+        string result2 = await DownloadContentAsync();
+        Console.WriteLine(result2);
+		
+        string result3 = await DownloadContentAsync();
+        Console.WriteLine(result3);
+    }
+	
+	async Task Method3()
+    {
+        string result1 = DownloadContent();
+        Console.WriteLine(result1);
+		
+        string result2 = await DownloadContentAsync();
+        Console.WriteLine(result2);
+		
+        string result3 = await DownloadContentAsync();
+        Console.WriteLine(result3);
+    }
+
+    static async Task<string> DownloadContentAsync()
+    {
+        using HttpClient client = new HttpClient();
+        string result = await client.GetStringAsync("http://example.com");
+        return result;
+    }
+
+    static string DownloadContent()
+    {
+        using HttpClient client = new HttpClient();
+        string result = client.GetString("http://example.com");
+        return result;
+    }
+}
